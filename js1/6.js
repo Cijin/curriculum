e6f5a808e968679dc6eb061b480cc1ae362d87db
cijin <cijininurhood@gmail.com>
2020-06-07 10:41:49 +0700

    removing js1 p6 from master

--- js1/6.js
+++ js1/6.js
@@ -7,14 +7,7 @@
  * @returns {boolean}
  */
 const solution = (num, i = 2) => {
-  if (num <= 1) { return false; }
   
-  for(i = 2;i <= num / 2; i++) {
-    if (num % i === 0) {
-      return false;
-    }
-  }  
-  return true;
 }
 
 module.exports = {
