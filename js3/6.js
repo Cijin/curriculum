9dabf5b90280aa529f3a50ae1704652543ee9cf6
cijin <cijininurhood@gmail.com>
2020-06-23 13:34:11 +0700

    better solution for js3 p6

--- js3/6.js
+++ js3/6.js
@@ -4,13 +4,12 @@
  * @returns {array}
 */
 
-const solution = (arr, res = []) => {
-  arr.forEach((e, i) => {
-    arr.forEach((el, idx) => {
-      if (e === el && i !== idx && !res.includes(e)) {
-        res.push(e);
-      }
-    });
+const solution = (arr, res = [], counter = {}) => {
+  arr.forEach((e) => {
+    counter[e] = (counter[e] || 0) + 1;
+    if (counter[e] > 1 && !res.includes(e)) {
+      res.push(e);
+    }
   });
   return res;
 };
