1728cdf21cd768e4b19585b2d574aa9ba6ab6067
cijin <cijininurhood@gmail.com>
2020-07-16 11:51:22 +0700

    solves js5 p1

--- js5/public/1/index.js
+++ js5/public/1/index.js
@@ -1,59 +1,65 @@
-const fetch = require('node-fetch');
-const fs = require('fs');
-const path = require('path');
-const express = require('express');
+//visit https://js5solutions.freedomains.dev/ for solution demo
+
+const fetch = require("node-fetch");
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
 const app = express();
-let visitor = {};
+
 let locationInfo = {};
-// DataStructure: 
-//   locationInfo ----> Object that has ip as key and count, Object 
-//     visitor as its info. Which further holds keys like 
+// DataStructure:
+//   locationInfo ----> Object that has ip as key and count, Object
+//     visitor as its info. Which further holds keys like
 //     cityStr, location (with latitude and longitude)
 
-app.get('/', (req, res) => {
-  res.send('This is a functioning url');
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("Go to /visitors and /api/visitors to see solution");
 });
 
 const getLocationInfo = async (ip) => {
   //for new ip's
   if (locationInfo[ip]) {
-    ++locationInfo[ip].visitor.visits; 
+    ++locationInfo[ip].visits;
     return locationInfo[ip];
   }
-  return fetch(`https://js5.c0d3.com/location/api/ip/${ip}`)
-  .then((response) => {
+  const location = await fetch(
+    `https://js5.c0d3.com/location/api/ip/${ip}`
+  ).then((response) => {
     return response.json();
-  }).then((location) => {
-    visitor.visits = 1;
-    visitor.cityStr = location.cityStr;
-    visitor["ll"] = location["ll"];
-    locationInfo[ip] = visitor;
-    fs.writeFile(path.resolve(__dirname, './visitorsFile'), JSON.stringify(locationInfo), () => {});
-    return visitor;
-  }).catch((error) => {
-    console.log("The following error occured: ", error);
-  });  
-}
+  });
 
-fs.readFile(path.resolve(__dirname, './visitorsFile'), (err, data) => {
+  const visitor = {};
+  visitor.visits = 1;
+  visitor.cityStr = location.cityStr;
+  visitor["ll"] = location["ll"];
+  locationInfo[ip] = visitor;
+  fs.writeFile(
+    path.resolve(__dirname, "./visitorsFile"),
+    JSON.stringify(locationInfo),
+    () => {}
+  );
+  return visitor;
+};
+
+fs.readFile(path.resolve(__dirname, "./visitorsFile"), (err, data) => {
   if (err) {
     return console.log("ReadFile Error: ", err);
   }
   if (data) {
-    locationInfo = JSON.parse(data.toString());   
-  }  
+    locationInfo = JSON.parse(data.toString());
+  }
 });
 
-app.use(express.static('public'));
-
-app.get('/visitors', (req, res) => { 
-  let ip = "::ffff:171.6.237.167";
-  getLocationInfo (ip).then((visitorInfo) => {
-
-  if (visitorInfo) {    
+app.get("/visitors", async (req, res) => {
+  let ip = req.get("x-forwarded-for");
+  console.log("req ip", req.ip);
+  const visitorInfo = await getLocationInfo(ip);
+  if (visitorInfo) {
     let allVistorsHtml = Object.keys(locationInfo).reduce((acc, key) => {
-      return acc += `<h3>${locationInfo[key].visitor.cityStr}: ${locationInfo[key].visitor.visits}</h3>`;
-    }, '');
+      return (acc += `<h3>${locationInfo[key].cityStr}: ${locationInfo[key].visits}</h3>`);
+    }, "");
 
     res.send(`
       <h1>You are visiting from ${visitorInfo.cityStr}</h1>
@@ -75,13 +81,12 @@ app.get('/visitors', (req, res) => {
       }
       </script>
       <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB29pGpCzE_JGIEMLu1SGIqwoIbc0sHFHo&callback=myMap"></script>
-    `)
+    `);
   }
-})
 });
 
-app.get('/api/visitors', (req, res) => {
-  res.sendFile(visitorsFile);
+app.get("/api/visitors", (req, res) => {
+  res.send(JSON.stringify(locationInfo));
 });
 
 app.listen(process.env.PORT || 3001);
