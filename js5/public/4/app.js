0767c3a0bc80403af1007024e20077956080fcf7
cijin <cijininurhood@gmail.com>
2020-07-20 18:25:22 +0700

    still solving js5 p4

--- js5/public/4/app.js
+++ js5/public/4/app.js
@@ -1,7 +1,10 @@
+const fs = require('fs');
 const path = require('path');
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+
+let files = {};
 
 app.use(bodyParser.json());
 
@@ -9,8 +12,32 @@ app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
+app.get('/files', (req, res) => {
+  fs.readFile(path.resolve(__dirname, "./fileInfo"), (err, data) => {
+    if (err) {
+      console.log("File read error");
+      return res.sendStatus(500);
+    }
+    if (data) {
+      return res.json([data]);
+    }
+  })
+});
+
 app.post('/api/files', (req, res) => {
-  
+  const name = req.body.name;
+  const content = req.body.content;
+  if (!name || !content) {
+    res.sendStatus(400);
+  }
+  files[name] = name;
+  files[name].content = content;
+  files[name].created = Date.now();
+  fs.writeFile(
+    path.resolve(__dirname, "./fileInfo"),
+    JSON.stringify(files), () => {}
+  );
+  res.sendStatus(201);
 });
 
 
