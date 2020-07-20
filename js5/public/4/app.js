a6f5740ce3ba0d1022f09c6e096b37a1a2f9545e
cijin <cijininurhood@gmail.com>
2020-07-20 17:51:02 +0700

    solving js5 p4

--- js5/public/4/app.js
+++ js5/public/4/app.js
@@ -1,2 +1,19 @@
+const path = require('path');
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT || 8123);
