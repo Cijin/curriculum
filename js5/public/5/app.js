2e1a6c5189306150713fe75c20721b3e7b3a0cbc
cijin <cijininurhood@gmail.com>
2020-07-24 13:28:31 +0700

    solving js5 p5

--- js5/public/5/app.js
+++ js5/public/5/app.js
@@ -11,6 +11,23 @@ app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
+app.get('/api/session', (req, res) => {
+  fetch('https://js5.c0d3.com/auth/api/session')
+    .then((res) => {
+      return res.json()
+    }).then((data) => {
+      return res.json({data});
+    })
+});
+
+app.get('/api/:roomName/messages', (req, res) => {
+
+});
+
+app.post('/api/:roomName/messages', (req, res) => {
+
+});
+
 app.listen(process.env.PORT || 8123, () => {
   console.log('Server listening on Port: 8123');
 });
