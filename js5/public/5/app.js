8b7094b67cea3d14bd89d8fac74153f0d9b8f33d
cijin <cijininurhood@gmail.com>
2020-07-26 13:14:49 +0700

    solves js5 p5

--- js5/public/5/app.js
+++ js5/public/5/app.js
@@ -25,18 +25,10 @@ const writeToFile = () => {
     () => {}
   );
 }
-
-app.use(bodyParser.json());
-app.use(morgan('dev'));
-
-app.get('/:roomName?', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'index.html'));
-});
-
-app.get('/api/session', (req, res) => {  
+//middleware to set user
+const session = (req, res, next) => {
   //get jwt token, to see if user is already logged in
   let jwtToken = req.get('authorization').split(" ")[1];
-
   fetch('https://js5.c0d3.com/auth/api/session', {
     headers: {
     'Authorization': `Bearer ${jwtToken}`
@@ -45,32 +37,50 @@ app.get('/api/session', (req, res) => {
       return response.json()
     }).then((data) => {
       if (!data.username) {
-        res.status = 403;
+        return res.status(403).json(data);
       }
-      return res.json(data);
+      req.user = data.username;
+      next();
     });
+}
+
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+app.get('/api/session', session, (req, res) => {
+  return res.status(200).json({ username: req.user });
 });
 
-app.get('/api/:roomName/messages', (req, res) => {
-  let roomName = req.params.roomName;
+app.get('/api/:roomName/messages', session, (req, res) => {
+  let roomName = req.params.roomName;  
   if (!chatRooms[roomName]) {
     chatRooms[roomName] = {};
     chatRooms[roomName].messages = [];
     writeToFile();    
   }  
-  return res.status(200).json({ messages: chatRooms[roomName].messages });
+  return res.status(200).json({ 
+    messages: chatRooms[roomName].messages    
+  });
 });
 
-app.post('/api/:roomName/messages', (req, res) => {
+app.post('/api/:roomName/messages', session, (req, res) => {
   const roomName = req.params.roomName;
-  const message = req.body.message;
+  const message = req.body.message;    
+
   if (!message) {
     return res.sendStatus(400);
   }
-  chatRooms[roomName].messages.push(message);
+  chatRooms[roomName].messages.push({
+    message: message,
+    username: req.user
+  });
   //update file
   writeToFile();
   return res.sendStatus(201);
+});
+
+app.get('/:roomName?', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
 app.listen(process.env.PORT || 8123, () => {
