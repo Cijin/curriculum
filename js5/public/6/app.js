c4f1d86c60a34e1e9537d77f72981b3c817f35ae
cijin <cijininurhood@gmail.com>
2020-07-26 09:47:51 +0700

    errors in js5 p5

--- js5/public/6/app.js
+++ js5/public/6/app.js
@@ -1,3 +1,22 @@
 const fs = require('fs');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const express = require('express');
 const app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.get('/api/sessions', (req, res) => {
+  const jwtToken = req.get('authorization').split(" ")[1];
+  if (!jwtToken) {
+    return res.status(400).send("Missing Authorization header.");
+  }
+  jwt.decode(jwtToken)
+});
+
+app.listen(process.env.PORT || 8123, () => {
+  console.log('Server listening on Port: 8123');
+});
