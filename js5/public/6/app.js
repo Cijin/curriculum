f8176938ce06f6dd84a209914aac62220db2fc3a
cijin <cijininurhood@gmail.com>
2020-07-28 13:50:26 +0700

    solves js5 p6

--- js5/public/6/app.js
+++ js5/public/6/app.js
@@ -2,67 +2,128 @@ const fs = require('fs');
 const path = require('path');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
-const uuid4 = require('uuid4');
+const { v4: uuidv4 } = require('uuid');
 const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
-const usersInfo = {};
-const secretKey = process.env.TOKEN_SECRET;
+let usersInfo = {};
+const secretKey = "Very Secret Key!";
 const saltRounds = 10;
 
-fs.readFile(path.resolve(__dirname, "userData"), (err, data) => {
-  if (err) {
-    console.log("Error reading file");
-  }
-  if (data) {
-    usersInfo = JSON.parse(data.toString());
-  }
-});
+const readFromFile = () => {
+  fs.readFile(path.resolve(__dirname, "userData"), (err, data) => {
+    if (err) {
+      console.log("Error reading file");
+    }
+    if (data) {
+      usersInfo = JSON.parse(data.toString());
+    }
+  });
+};
 
-const writeFile = () => {
+const writeToFile = () => {
   fs.writeFile(path.resolve(__dirname, "userData"),
     JSON.stringify(usersInfo),
     () => {});
 };
 
-const session = (req, res) => {
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cors({
+  origin: 'https://js5p5.freedomains.dev'
+}));
+
+app.get('/', (req, res) => {
+  res.send("Working Auth Url");
+});
+
+app.get('/api/sessions', (req, res) => {
   if (!req.get('authorization')) {
     return res.status(403).send("Missing Authorization Header");
   }
   const token = req.get('authorization').split(" ")[1];
-  jwt.verify(token, secretKey, (err, data) => {
-    if (err) {
-      return res.sendStatus(403);
-    }
-    return res.status(200).json({data});
-  });
-};
+  const decoded = jwt.decode(token);
 
-const logIn = (req, res) => {
+  if (!decoded) {
+    return res.sendStatus(403);
+  }
+  return res.status(200).json(decoded);
+});
+
+app.post('/api/sessions', (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
+  
+  readFromFile();
 
-  if (!username || !password) {
-    return res.status(400).send("Missing username or password");
+  if (!usersInfo[username]) {
+    return res.status(400).send("Please enter a valid username");
   }
-  const user = { username, password };
 
-  jwt.sign({user}, secretKey, (err, token) => {
-    res.status(200).json({token});
+  if (!password || !username) {
+    return res.status(400).send("Please enter both username and password");
+  }
+  
+  bcrypt.compare(password, usersInfo[username].password, (error, result) => {
+    if (error) {
+      console.log(error);
+      return;
+    }
+    if (!result) {
+      return res.sendStatus(403);
+    }
+    let user = usersInfo[username];
+    user.token = jwt.sign({
+      id: user.id,
+      username: username
+    }, secretKey);
+    usersInfo[username] = user;
+    writeToFile();
+    delete user.password;
+    return res.status(200).json(user);
   });
-};
+});
 
-const signUp = (req, res) => {
+app.post('/api/users', (req, res) => {
+  const body = req.body;
+  const email = req.body.email;
+  const username = req.body.username;
+  const password = req.body.password;
+  const id = uuidv4();
 
-};
+  if(!email.match(/^\S+@\S+$/)) {
+    return res.status(400).send("Please enter a valid email!");
+  }
+  if(usersInfo[username] || !username) {
+    return res.status(400).send("Please try another username");
+  }
 
-const logOut = () => {
+  //finding length of original string from base64
+  if((3 * Math.ceil(password.length / 4) - (password.match(/=/g) || [])).length < 5) {
+    return res.status(400).send("Password should contain at least 5 characters");
+  }
 
-};
+  bcrypt.hash(password, saltRounds)
+    .then((hash) => {
+      body.password = hash;
+      body.token = jwt.sign({id, username}, secretKey);
+      let user = Object.entries(body).reduce((acc, [key, value]) => {
+        acc[key] = value;
+        return acc;
+      }, {});
+      usersInfo[username] = user;
+      writeToFile();
+      //deleting password before sending user info
+      delete user.password;
+      return res.status(200).json(user);
+    }).catch((error) => {
+      console.log('The following error was thrown: ', error);
+    });
+});
 
-module.exports = {
-  signUp,
-  logIn,
-  session,
-  logOut
-};
+app.listen(process.env.PORT || 8123, () => {
+  console.log('App listening on Port: 8123');
+});
