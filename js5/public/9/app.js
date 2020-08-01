62a6172f4e1c4632b95414ad3fb5ae8698b3f379
cijin <cijininurhood@gmail.com>
2020-08-01 13:29:59 +0700

    simple login added, api needs update

--- js5/public/9/app.js
+++ js5/public/9/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
@@ -6,6 +7,19 @@ const app = express();
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
+app.get('/api/session', (req, res) => {
+  //TODO: decode jwt token to return a json body with username
+});
+
+app.post('/api/session', (req, res) => {
+  //TODO: create a new jwt token for the current user
+  //or return an existing one if one exists  
+});
 
 app.listen(process.env.PORT || 8123, () => {
   console.log('Server listening on Port: 8123');
