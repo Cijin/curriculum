6d61904ce47581ac1036d76ad1ca3d411ab565e8
cijin <cijininurhood@gmail.com>
2020-08-08 21:57:03 +0700

    solves js6 p1

--- js6/public/1/app.js
+++ js6/public/1/app.js
@@ -1,5 +1,43 @@
 const { ApolloServer, gql } = require('apollo-server-express');
-const { typeDefs, resolvers } = require('./schema');
+const express = require('express');
+const app = express();
+const fetch = require('node-fetch');
+
+const typeDefs = gql`
+  type Lesson {
+    title: String
+  }
+
+  type Query {
+    lessons: [Lesson]
+    getPokemon(id: Int!): Pokemon
+    search(str: String!): Pokemon
+  }
+
+  type Pokemon {
+    name: String
+    image: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    lessons: async () => {
+      const response = await fetch("https://c0d3.com/api/lessons");
+      return response.json();
+    },
+
+    getPokemon: async (parent, { id }) => {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+      return response.json();
+    },
+
+    search: async (parent, {str}) => {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${str}`);
+      return response.json();
+    }
+  },
+};
 
 const server = new ApolloServer({
   typeDefs,
@@ -8,6 +46,6 @@ const server = new ApolloServer({
 
 server.applyMiddleware({ app });
 
-app.listen({ port: 4000 }, () => {
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+app.listen({ port: 4001 }, () => {
+  console.log(`🚀 Server ready at http://localhost:4001${server.graphqlPath}`);
 });
