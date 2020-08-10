fcab585c953a95333b68d328689d1dce7adbfcea
cijin <cijininurhood@gmail.com>
2020-08-10 16:53:52 +0700

    schema updated

--- js6/public/2/app.js
+++ js6/public/2/app.js
@@ -1,4 +1,17 @@
 const express = require('express');
 const app = express();
-const { ApolloServer } = require('apollo-server-express');
+const session = require('express-session');
 const fetch = require('node-fetch');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schema');
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+server.applyMiddleware({ app });
+
+app.listen({ port: 4001 }, () => {
+  console.log(`🚀 Server ready at http://localhost:4001/graphql}`);
+});
--- js6/public/2/schema.js
+++ js6/public/2/schema.js
@@ -1,5 +1,59 @@
 const { gql } = require('apollo-server-express');
 
-const typeDefs = gql`
-  
+export default typeDefs = gql`
+  type Pokemon {
+    name: String
+    image: String
+  }
+
+  type BasicPokemon {
+    name: String 
+  }
+
+  type Lesson {
+    title: String
+  }
+
+  type User {
+    name: String
+    image: String
+    lessons: [Lesson]
+  }
+
+  type Query {
+    lessons: [Lesson]
+    search(str: String!): [BasicPokemon]
+    getPokemon(str: String!): Pokemon
+    user: User
+    login(pokemon: String!): User
+  }
+
+  type Mutation {
+    enroll(title: String!): User
+    unenroll(title: String!): User
+  }
 `;
+
+const resolvePokemon = async (parent, {str}) => {
+  return await fetch(`https://pokeapi.co/api/v2/pokemon/${str}`)
+    .then((response) => response.json())
+    .then((data) => {
+      return {
+        name: data.name,
+        image: data.sprites.front_default
+      }
+    })
+};
+
+export default resolvers = {
+  Query: {
+    lessons: async () => {
+      const response = await fetch("https://c0d3.com/api/lessons");
+      return response.json();
+    },
+
+    search: resolvePokemon,
+
+    getPokemon: resolvePokemon,
+  }
+};
