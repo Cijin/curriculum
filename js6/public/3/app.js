a5d94c6ae251677a093c4678d460fb0f47d0d555
cijin <cijininurhood@gmail.com>
2020-08-17 14:31:52 +0700

    stars completed thanks to song

--- js6/public/3/app.js
+++ js6/public/3/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'kanban.html'));
