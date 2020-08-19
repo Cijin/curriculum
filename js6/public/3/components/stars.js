79fb5b15ac41510879bbfdc53fdfc025f0ebcb33
cijin <cijininurhood@gmail.com>
2020-08-20 00:00:42 +0700

    webpack set up, now just to build the front end

--- js6/public/3/components/stars.js
+++ js6/public/3/components/stars.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 function Stars() {
   const [value, setValue] = useState(0);
---
+++ js6/public/3/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const morgan = require('morgan');
+
+app.use(morgan('dev'));
+app.use(express.static(path.resolve(__dirname, 'dist')));
+
+app.get('/', (req, res) => {
+  return res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
+app.listen({port:8123}, () => {
+  console.log('Server running on Port: 8123');
+});
