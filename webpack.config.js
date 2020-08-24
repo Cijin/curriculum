5f2d308a55029cd15c3c67d70737c4d9e1833e81
cijin <cijininurhood@gmail.com>
2020-08-24 11:58:20 +0700

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,21 @@
 const path = require('path');
-  2 
-  3 module.exports  = {
-  4   entry: './js6/public/4/components/app.js',
-  5   mode: 'development',
-  6   output: {
-  7     path: path.resolve(__dirname, 'js6/public/4/dist'),
-  8   },
-  9   module: {
- 10     rules: [
- 11       {
- 12         test: /\.js$/,
- 13         exclude: '/node_modules',
- 14         loader: 'babel-loader',
- 15         options: {
- 16           'presets': ['@babel/preset-react']
- 17         },
- 18       },
- 19     ],
- 20   },
- 21 };
+ 
+module.exports  = {
+  entry: './js6/public/4/components/app.js',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'js6/public/4/dist'),
+  },
+  module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: '/node_modules',
+          loader: 'babel-loader',
+          options: {
+            'presets': ['@babel/preset-react']
+          },
+        },
+      ],
+    },
+};
