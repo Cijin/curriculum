7f163de1b982f070654ab11491d6d57948a5cc43
cijin <cijininurhood@gmail.com>
2020-08-25 21:47:12 +0700

    shall continue in a bit

---
+++ js6/public/5/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+app.use(morgan('dev'));
+
+app.get('/images', (req, res) => {
+  res.send('Request Recieved');
+});
+
+app.listen({ port: 8123 }, () => {
+  console.log('Server listening on Port: 8123');
+});
