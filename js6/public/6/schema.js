9324565d94a7b0e3ed7ec0382ac41641a2e3d621
cijin <cijininurhood@gmail.com>
2020-08-28 17:08:47 +0700

    schema.js updated

--- js6/public/6/schema.js
+++ js6/public/6/schema.js
@@ -2,26 +2,22 @@ const { gql } = require('apollo-server-express');
 const fetch = require('node-fetch');
 const { allPokemons } = require('../server');
 let allLessons = {};
-let userInfo = {};
 let userCache = {};
+let userInfo = {};
 
 const typeDefs = gql`
   type Lesson {
     title: String
-    rating: Int
   }
-
   type Mutation {
     enroll(title: String!): User
     unenroll(title: String!): User
-    setRating(title: String, value: Int): User
+    setRating(title: String, rating: Int): User
   }
-
   type Pokemon {
     name: String
     image: String
   }
-
   type Query {
     lessons: [Lesson]
     search(str: String!): [Pokemon]
@@ -29,14 +25,12 @@ const typeDefs = gql`
     user: User
     login(pokemon: String!): User
   }
-
   type User {
     name: String
     image: String
     lessons: [Lesson]
   }
 `;
-
 const resolvers = {
   Query: {
     lessons: async () => {
@@ -49,89 +43,67 @@ const resolvers = {
       }
       return allLessons;
     },
-
     getPokemon: (_, { str }) => {
       return allPokemons[str];
     },
-
     search: (_, { str }) => {
       return allPokemons.filter((pokemon) => {
         return pokemon.name.includes(str);
       });
     },
-
     login: (_, { pokemon }, { req }) => {
       if (!userCache[pokemon]) {
         req.session.user = pokemon;
         userCache[pokemon] = {};
         userCache[pokemon].name = allPokemons[pokemon].name;
         userCache[pokemon].image = allPokemons[pokemon].image;
-        const lessons = userInfo[pokemon].lessons || {};
-
-        userCache[pokemon].lessons = Object.entries(lessons).map(
-          ([title, rating]) => {
-            return { title, rating };
-          }
-        );
+        userCache[pokemon].lessons = {};
       }
       return userCache[pokemon];
     },
-
     user: (_, _, { req }) => {
-      const pokemon = req.session.user;
-      return userCache[pokemon];
+      return req.session.user || {};
     },
   },
-
   Mutation: {
-    enroll: (_, { title, value }, { req }) => {
+    enroll: (_, { title }, { req }) => {
       const pokemon = req.session.user;
       if (!pokemon) return;
 
-      const lessons = userCache[pokemon].lessons || {};
-      lessons[title] = value;
+      const lessons = userInfo[pokemon].lessons || {};
+      lessons[title] = 0;
       userCache[pokemon].lessons = Object.entries(lessons).map(
         ([title, rating]) => {
           return { title, rating };
         }
       );
-      userInfo[pokemon].lessons = {};
-      userInfo[pokemon].lessons[title] = value;
-
+      userInfo[pokemon].lessons = lessons;
       return userCache[pokemon];
     },
-
     unenroll: (_, { title }, { req }) => {
       const pokemon = req.session.user;
       if (!pokemon) return;
 
-      delete userInfo[pokemon].lessons[title];
       const lessons = userInfo[pokemon].lessons;
-      userCache[pokemon].lessons = Object.entries(lessons).map(
-        ([title, rating]) => {
-          return { title, rating };
-        }
-      );
-
+      delete lessons[title];
+      userInfo[pokemon].lessons = lessons;
+      userCache[pokemon].lessons = Object.entries.map(([title, rating]) => {
+        return { title, rating };
+      });
       return userCache[pokemon];
     },
-  },
+    setRating: (_, { title, rating }, { req }) => {
+      const pokemon = req.session.user;
+      if (!pokemon) return;
 
-  setRating: (_, { title, value }, { req }) => {
-    const pokemon = req.session.pokemon;
-    if (!pokemon) return;
+      userInfo[pokemon].lessons[title] = rating;
+      const lessons = userInfo[pokemon].lessons;
 
-    userInfo[pokemon].lessons[title] = value;
-    const lessond = userInfo[pokemon].lessons;
-
-    userCache[pokemon].lessons = Object.entries(lessons).map(
-      ([title, rating]) => {
+      userCache[pokemon].lessons = Object.entries.map(([title, rating]) => {
         return { title, rating };
-      }
-    );
-
-    return userCache[pokemon];
+      });
+      return useCache[pokemon];
+    },
   },
 };
-
 module.exports = { typeDefs, resolvers };
