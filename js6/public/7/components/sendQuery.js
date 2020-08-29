7cfef8037cc85957bdf80c83b03a956f156beab6
cijin <cijininurhood@gmail.com>
2020-08-29 20:46:50 +0700

    file structure good to go

---
+++ js6/public/7/components/sendQuery.js
@@ -0,0 +1,17 @@
+const sendQuery = (query) => {
+  return fetch('https://js5.c0d3.com/js6c2/graphql', {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      operationName: null,
+      variables: {},
+      query,
+    }),
+  })
+    .then((r) => r.json())
+    .then((r) => r.data);
+};
+
+export default sendQuery;
