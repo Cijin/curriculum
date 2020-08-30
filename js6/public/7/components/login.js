343ca148a9120a1f183f9bf766489b1b8015d137
cijin <cijininurhood@gmail.com>
2020-08-30 09:13:45 +0700

    updated login to render some data

--- js6/public/7/components/login.js
+++ js6/public/7/components/login.js
@@ -3,5 +3,10 @@ import { useHistory } from 'react-router-dom';
 import sendQuery from './sendQuery';
 
 function Login(props) {
-  
+  return (
+    <div>
+      <h1>Pokemon Search</h1>
+      <input className="searcBox" />
+    </div>
+  );
 }
