bb70af45b733b6188c6dfaf3207633799963e472
cijin <cijininurhood@gmail.com>
2020-08-30 13:00:18 +0700

    login page complete

--- js6/public/7/components/login.js
+++ js6/public/7/components/login.js
@@ -11,17 +11,46 @@ const debounce = (fn, time) => {
   };
 };
 
+const loadPokemon = async (pokemon) => {
+  return await sendQuery(
+    `{ getPokemon (str: "${pokemon}") { name, image }}`
+  ).then((data) => data.getPokemon);
+};
+
 function Login(props) {
   const inputEl = useRef(null);
   const [suggestions, setSuggestions] = useState('');
+  const [pokemon, setPokemon] = useState({ name: '', image: '' });
 
-  const handleClick = () => {};
+  const handleKeyDown = async (e) => {
+    if (e.keyCode === 13 && e.target.value) {
+      const pokeObject = await loadPokemon(e.target.value);
+      setPokemon({
+        name: pokeObject.name,
+        image: pokeObject.image,
+      });
+      setSuggestions('');
+      inputEl.current.value = '';
+    }
+    return;
+  };
 
   const handleChange = debounce(() => {
     const str = inputEl.current.value;
     sendQuery(`{ search(str: "${str}") { name } }`).then((data) => {
       const results = data.search || [];
+
       const names = results.map((pokemon, idx) => {
+        const handleClick = async () => {
+          const obj = await loadPokemon(pokemon.name);
+          setPokemon({
+            name: obj.name,
+            image: obj.image,
+          });
+          setSuggestions('');
+          inputEl.current.value = '';
+        };
+
         return (
           <h3 onClick={handleClick} key={pokemon.name + idx}>
             {reactStringReplace(pokemon.name, str, (match, idx) => (
@@ -39,8 +68,18 @@ function Login(props) {
   return (
     <div>
       <h1>Pokemon Search</h1>
-      <input onChange={handleChange} ref={inputEl} className="searchBox" />
-      <div>{suggestions}</div>
+      <input
+        onKeyDown={handleKeyDown}
+        onChange={handleChange}
+        ref={inputEl}
+        className="searchBox"
+      />
+      <div className="suggestions">{suggestions}</div>
+      <div>
+        <h3>{pokemon.name}</h3>
+        <img src={pokemon.image} />
+        {pokemon.name && <button className="loginButton">Login</button>}
+      </div>
     </div>
   );
 }
