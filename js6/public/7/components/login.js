1aa891e204ead9d3cf0a2ec897784a215adab37c
cijin <cijininurhood@gmail.com>
2020-08-30 10:28:53 +0700

    added suggestions

--- js6/public/7/components/login.js
+++ js6/public/7/components/login.js
@@ -1,6 +1,7 @@
 import React, { useState, useRef } from 'react';
 import { useHistory } from 'react-router-dom';
 import sendQuery from './sendQuery';
+import reactStringReplace from 'react-string-replace';
 
 const debounce = (fn, time) => {
   let timeout;
@@ -11,10 +12,33 @@ const debounce = (fn, time) => {
 };
 
 function Login(props) {
+  const inputEl = useRef(null);
+  const [suggestions, setSuggestions] = useState('');
+
+  const handleChange = debounce(() => {
+    const str = inputEl.current.value;
+    sendQuery(`{ search(str: "${str}") { name } }`).then((data) => {
+      const results = data.search || [];
+      const names = results.reduce((acc, name, idx) => {
+        return (acc += (
+          <h3 onClick={handleClick}>
+            {reactStringReplace(name, str, (match, i) => (
+              <span className="match" key={i}>
+                {match}
+              </span>
+            ))}
+          </h3>
+        ));
+      }, '');
+      setSuggestion(names);
+    });
+  }, 500);
+
   return (
     <div>
       <h1>Pokemon Search</h1>
-      <input className="searchBox" />
+      <input ref={inputEl} className="searchBox" />
+      <div>{suggestions}</div>
     </div>
   );
 }
