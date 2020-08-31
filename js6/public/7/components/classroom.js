2651ccf919b029724512c209c5bee9fa22cc272c
cijin <cijininurhood@gmail.com>
2020-08-31 14:36:36 +0700

    add star component and setrating mutation to gql next

--- js6/public/7/components/classroom.js
+++ js6/public/7/components/classroom.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import sendQuery from './sendQuery';
+import Lessons from './lessons';
 
 function Classroom(props) {
   const [user, setUser] = useState({
@@ -7,28 +8,28 @@ function Classroom(props) {
     image: '',
     lessons: [],
   });
-  const [unenrolled, setUnenrolled] = useState([]);
+  const [lessons, setLessons] = useState([]);
 
   useEffect(() => {
     sendQuery(
       `{ user { name, image, lessons { title } } lessons { title } }`
-    ).then(
-      (data) => {
-        if (data && data.user) {
-          setUser(data.user);
-        } else {
-          setUser({});
-        }
-        setUnenrolled(data.lessons);
-      },
-      [user, unenrolled]
-    );
-  });
+    ).then((data) => {
+      if (data && data.user) {
+        setUser(data.user);
+      } else {
+        setUser({});
+      }
+      setLessons(data.lessons);
+    });
+  }, []);
 
   return (
     <div>
       <h1>{user.name}</h1>
       <img src={user.image} />
+      <div>
+        <Lessons enrolledObj={user.lessons} lessons={lessons} />
+      </div>
     </div>
   );
 }
--- js6/public/7/components/lessons.js
+++ js6/public/7/components/lessons.js
@@ -1,21 +1,61 @@
-import React from 'react';
-import Star from 'stars';
+import React, { useState } from 'react';
+//import Star from 'stars';
 import sendQuery from './sendQuery';
 
-function Lessons(props) {
-  const lessons = props.lessons.map((lesson, idx) => {
-    const handleClick = () => {
-      props.handleClick(lesson);
+function Lessons({ enrolledObj, lessons }) {
+  const enrolled = enrolledObj.map((lesson) => lesson.title);
+  const [enrolledLessons, setEnrolledLessons] = useState(enrolled);
+
+  const unenrolled = lessons.filter((lesson) => {
+    return !enrolledLessons.includes(lesson.title);
+  });
+
+  const unenrolledComponent = unenrolled.map((lesson, idx) => {
+    const enroll = () => {
+      sendQuery(`
+        mutation { enroll (title: "${lesson.title}") { name } }
+      `).then(() => {
+        setEnrolledLessons([...enrolledLessons, lesson.title]);
+      });
     };
 
     return (
-      <h3 key={idx} onClick={}>
+      <h3 key={idx} onClick={enroll}>
+        {lesson.title}
+      </h3>
+    );
+  });
+
+  const enrolledComponent = enrolledLessons.map((lesson, idx) => {
+    const unenroll = () => {
+      sendQuery(`
+        mutation { enroll (title: "${lesson}") { name } }
+      `).then(() => {
+        const newLessons = [...enrolledLessons];
+        newLessons.splice(idx, 1);
+        setEnrolledLessons(newLessons);
+      });
+    };
+
+    return (
+      <h3 key={idx} onClick={unenroll}>
         {lesson}
       </h3>
     );
   });
 
-  return <div>{lessons}</div>;
+  return (
+    <div className="lessons">
+      <div>
+        <h1>Enrolled Lessons</h1>
+        {enrolledComponent}
+      </div>
+      <div>
+        <h1>Unenrolled Lessons</h1>
+        {unenrolledComponent}
+      </div>
+    </div>
+  );
 }
 
 export default Lessons;
